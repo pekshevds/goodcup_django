bd18841a7028965e44de84e38c1fc96d4e4b4e76
denis sergeevich pekshev <pekshev.ds@gmail.com>
2025-09-19 10:58:42 +0300

    refactoring

--- server/settings.py
+++ server/settings.py
@@ -101,11 +101,8 @@ else:
             "PASSWORD": environ.get("DB_PASSWORD"),
             "OPTION": {
                 "init_command": "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-                # "init_command": "SET sql_mode='STRICT_ALL_TABLES'",
                 "charset": "utf8mb4",
                 "autocommit": True,
-                # "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-                # "use_pure": True,
             },
         }
     }
