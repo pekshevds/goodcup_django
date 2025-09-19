bb1cb8f2e88712db6d40cc09c62394481ac7be54
denis sergeevich pekshev <pekshev.ds@gmail.com>
2025-09-19 07:51:11 +0300

    change db connection settings

--- server/settings.py
+++ server/settings.py
@@ -100,9 +100,11 @@ else:
             "USER": environ.get("DB_USER", ""),
             "PASSWORD": environ.get("DB_PASSWORD", ""),
             "OPTION": {
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-                "charset": "utf8mb4",
-                "autocommit": True,
+                # "init_command": "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                # "charset": "utf8mb4",
+                # "autocommit": True,
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                "use_pure": True,
             },
         }
     }
